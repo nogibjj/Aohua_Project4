e8c173dcf1cf633c4f92500f31a943f232b1aa1f
Aohua Zhang <83438699+OliverZhangA@users.noreply.github.com>
2022-11-28 02:39:15 +0000

    add weather and event search

--- fastapi-app.py
+++ fastapi-app.py
@@ -1,18 +1,42 @@
 from fastapi import FastAPI
 import uvicorn
 #from dblib.querydb import querydb
-from readapi import getjson
+from readapi import geteventsasjson, getweather
+import json
 
 app = FastAPI()
 
 @app.get("/events")
 async def getevents():
-    jsondata = getjson()
+    jsondata = geteventsasjson(-1, "", "")
+    return jsondata
+
+@app.get("/events/{days}")
+async def geteventsindays(days: int):
+    jsondata = geteventsasjson(days, "", "")
+    return jsondata
+
+@app.get("/events/{days}/{attribute}/{keyword}")
+async def geteventsintarget(days: int, attribute: str, keyword: str):
+    jsondata = geteventsasjson(days, attribute, keyword)
     return jsondata
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Databricks"}
+    #return {"message": "Hello Databricks"}
+    weatherinfo = getweather()
+    # Data to be written 
+    dictionary ={ 
+        "Greetings": weatherinfo,
+        "Check all events": "/events",
+        "Check events in future n days": "/events/n",
+        "Search target key word in attribute": "/events/n/attribute/keyword"
+    } 
+        
+    # Serializing json  
+    return dictionary
+    #return {"Greetings": }
+    return getweather()
 
 
 @app.get("/subt/{num1}/{num2}")
